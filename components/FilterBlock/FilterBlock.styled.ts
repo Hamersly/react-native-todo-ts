05962f1ac12b27e9ba66e9e1fe82014225bf6f02
Hamersly <maksimwanderer96@gmail.com>
2022-09-16 01:29:00 +0500

    rename project

--- components/FilterBlock/FilterBlock.styled.ts
+++ components/FilterBlock/FilterBlock.styled.ts
@@ -4,12 +4,14 @@ export const Filters = styled.View`
   display: flex;
   flex-direction: row;
   justify-content: space-around;
-  margin: 20px;
+  margin: 20px 20px 5px 20px;
+  border-bottom-color: black;
+  border-bottom-width: 2px;
+  padding-bottom: 20px;
 `;
 
 export const FilterButton = styled.Pressable<{ color: string }>`
   background-color: ${props => props.color};
   padding: 10px;
   border-radius: 30px;
-  elevation: 1;
 `;
